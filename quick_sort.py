01d5a516d36053c822a508b72b27d03a77412164
Mehmet Gerceker <mehmetg@gmail.com>
2015-01-06 20:41:40 -0800

    Updated partitioning removing unnecessary code and properly allowing pivot selection.

--- quick_sort.py
+++ quick_sort.py
@@ -1,7 +1,7 @@
 
 import random
 
-def partition(arr, start, end, pivot = None):
+def partition_old(arr, start, end, pivot = None):
 	if pivot == None: pivot = arr[end]
 	left = start
 	right = end - 1
@@ -14,17 +14,39 @@ def partition(arr, start, end, pivot = None):
 			arr[left], arr[right] = arr[right], arr[left]
 	arr[end], arr[left] = arr[left], arr[end]
 	return left
-def _qsort(arr, start, end):
+def qsort_old(arr, start, end):
 	if start >= end:
 		return
 	p = partition(arr, start, end)
-	_qsort(arr,start,p-1)
-	_qsort(arr,p+1, end)
+	qsort(arr,start,p-1)
+	qsort(arr,p+1, end)
+
+def partition(arr, start, end, pivot = None):
+	if pivot == None: pivot = arr[end]
+	while start < end:
+		while (start < end) and (arr[start] < pivot): start += 1
+		while (start < end) and (arr[end] > pivot): end -= 1
+		if(arr[start] > arr[end]):
+			arr[start], arr[end] = arr[end], arr[start]
+	return start
+
+def qsort(arr, start, end):
+	if(start >= end):
+		return
+	p_idx = partition(arr,start,end, arr[(start+end)//2])
+	qsort(arr, start, p_idx - 1)
+	qsort(arr, p_idx + 1, end)
+
 def main():
 	a = range(-15,16)
+	
+	#a = a + a[:len(a)//4] + a[(len(a)//2):]
+	
 	random.shuffle(a)
 	print(a)
-	_qsort(a,0,len(a)-1)
+	#print(partition(a,0,len(a)-1))
+	#print(a)
+	qsort(a,0,len(a)-1)
 	print(a)
 
 if __name__ == '__main__':
