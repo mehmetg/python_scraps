1b7a88705907117a9e7dd570dbbfba3e15dbfc63
Mehmet Gerceker <mehmetg@gmail.com>
2015-01-15 00:18:34 -0800

    Bin search tree building in order printing etc is done. Can tell if tree is balanced. 3.1 is done.

---
+++ trees_graphs.py
@@ -0,0 +1,65 @@
+class TreeNode(object):
+	
+	def __init__(self, payload = None, children = 2):
+		self.data = payload
+		self.visited = False
+		#left right, etc
+		self.adjacent = [None] * children
+def add_to_bin_search_tree(root, pl):
+	if root and root.data:
+		if pl > root.data:
+			if(root.adjacent[1] is None):
+				root.adjacent[1] = TreeNode()
+			add_to_bin_search_tree(root.adjacent[1],pl)
+		else:
+			if(root.adjacent[0] is None):
+				root.adjacent[0] = TreeNode()
+			add_to_bin_search_tree(root.adjacent[0],pl)
+	else:
+		root.data = pl
+def build_bin_search_tree(arr):
+	root = TreeNode()
+	for x in arr:
+		add_to_bin_search_tree(root, x)
+	return root
+def print_bin_tree_in_order(root):
+	if root:
+		print_bin_tree_in_order(root.adjacent[0])
+		print(root.data)
+		print_bin_tree_in_order(root.adjacent[1])
+def _is_bin_tree_balanced(root, threshold = 1):
+	if root:
+		r = _is_bin_tree_balanced(root.adjacent[0], threshold)
+		#print(r)
+		if r == -1: return -1
+		l = _is_bin_tree_balanced(root.adjacent[1], threshold)
+		#print(l)
+		if l == -1 : return -1
+		if abs(r-l) > threshold: 
+			return -1
+		else:
+			return (max(r,l) + 1)
+	else:
+		return 0
+def is_bin_tree_balanced(root, threshold = 1):
+	if _is_bin_tree_balanced(root, threshold) == -1:
+		return False
+	else:
+		return True
+
+def main():
+	import random
+	r = False
+	a = range(11)
+	while not r:
+		random.shuffle(a)
+		#print(a)
+		tree = build_bin_search_tree(a)
+		#print_bin_tree_in_order(tree)
+		r = is_bin_tree_balanced(tree, 1)
+	
+	print(r)
+	print(a)
+
+if __name__ == '__main__':
+	main()
